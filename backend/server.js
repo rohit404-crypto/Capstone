857d4bfe99a2aed43c205edf2471c06c31bb9f6b
Rohit Kumar Pathak <84014109+rohit404-crypto@users.noreply.github.com>
2023-12-21 14:05:03 +0530

    backend capstone build

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Usermodel = require("./models");
+const Usermodel = require("./models/models");
 const bcrypt = require("bcrypt");
+const Productmodel = require("./models/productmodel")
+const Catmodel = require("./models/CatModel");
+const Fishmodel = require("./models/FishModel");
 
 const app = express();
 app.use(express.json());
@@ -78,6 +81,109 @@ try{
 }
  
 });
+app.post("/api/postdogproducts",async(req,res)=>{
+  try{
+  const Products = await Productmodel.create({
+    
+    name: req.body.name,
+    imageurl: req.body.imageurl,
+    price: req.body.price,
+    rating:req.body.rating
+
+  });
+  return res.status(200).send({status:"ok"});
+}catch(error){
+  return res.status(500).send("internal server error")
+}
+})
+
+app.get("/api/dogproducts", async (req, res) => {
+  try {
+    const productmodels = await Productmodel.find();
+    return res.status(200).json(productmodels);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+app.post("/api/postcatproducts",async(req,res)=>{
+  try{
+  const CatProducts = await Catmodel.create({
+    
+    name: req.body.name,
+    imageurl: req.body.imageurl,
+    price: req.body.price,
+    rating:req.body.rating,
+    desc:req.body.desc
+
+  });
+  return res.status(200).send({status:"ok"});
+}catch(error){
+  return res.status(500).send("internal server error")
+}
+})
+
+app.get("/api/catproducts", async (req, res) => {
+  try {
+    const catmodels = await Catmodel.find();
+    return res.status(200).json(catmodels);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+app.post("/api/postfishproducts",async(req,res)=>{
+  try{
+  const FishProducts = await Fishmodel.create({
+    
+    name: req.body.name,
+    imageurl: req.body.imageurl,
+    price: req.body.price,
+    rating:req.body.rating,
+    desc:req.body.desc
+
+  });
+  return res.status(200).send({status:"ok"});
+}catch(error){
+  return res.status(500).send("internal server error")
+}
+})
+
+app.get("/api/fishproducts", async (req, res) => {
+  try {
+    const fishmodels = await Fishmodel.find();
+    return res.status(200).json(fishmodels);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
+
+app.get("/api/petproducts",async(req,res)=>{
+  try{
+    const fishmodels = await Fishmodel.find();
+    const dogmodels = await Productmodel.find();
+    const catmodels = await Catmodel.find();
+
+    return res.status(200).json({fishmodels,dogmodels,catmodels})
+  }
+  catch(err){
+    return res.status(500).send("Internal Server Error");
+
+  }
+})
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("server started on port 3000");
